55f12d83c1fb6d204a10b94df749b0e59c6d41bc
aayushs22 <121499193+aayushs22@users.noreply.github.com>
2023-06-21 23:29:44 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    registerGen(argc, argv, 1);
+    int n = atoi(argv[1]);
+    int m=  atoi(argv[2]);
+    int ai= atoi(argv[3]);
+    // long long n=100000;
+    cout << n << " ";
+    cout << m << endl;
+    for(int i=0;i<n;i++){
+       if(i!=n-1) cout<<rnd.next(1,ai)<<" ";
+       else cout<<rnd.next(1,ai);
+    }
+    cout<<endl;
+}
---
+++ vali.cpp
@@ -0,0 +1,27 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+// bool prime(int x){
+//   if(x == 1)return false;
+// 	for(int i = 2;i * i <= x;i++){
+// 		if(x%i == 0)return false;
+// 	}
+// 	return true;
+// }
+int main(int argc, char** argv){
+    registerValidation(argc, argv);
+    int n=inf.readInt(1,100000,"n");
+    inf.readSpace();
+    int m=inf.readInt(1,100000,"m");
+    // inf.readSpace();
+    inf.readEoln();
+    for(int i=1;i<=n;i++){
+      int ai=inf.readInt(1,1000000000,"ai");
+      if(i!=n){
+        inf.readSpace();
+      }
+    }
+    inf.readEoln();
+    inf.readEof();
+    return 0;
+}
